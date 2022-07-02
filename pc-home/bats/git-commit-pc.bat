ecbf05f135bf7f68064031caa503adc45f48d7d8
guowenchaode <guowenchaode@126.com>
2022-07-02 09:51:34 +0800

    update-2022-07-02

--- pc-home/bats/git-commit-pc.bat
+++ pc-home/bats/git-commit-pc.bat
@@ -1,12 +1,10 @@
-cd D:\__Alex
-git add .
-git commit . -m update-%date%
-git push
+git add D:\__Alex
+git commit D:\__Share -m update-%date%
+git push D:\__Share
 
 
-cd D:\__Share
-git add .
-git commit . -m update-%date%
-git push
+git add D:\__Share
+git commit D:\__Share -m update-%date%
+git push D:\__Share
 
 cmd  /k echo %date%
